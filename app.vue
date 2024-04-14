683c855cb5333262fbdb463cd8ece10f61bf701e
Luka Grabnar <grabnar.luka@gmail.com>
2024-04-14 02:08:50 +0200

    left/right sidebar

--- app.vue
+++ app.vue
@@ -1,5 +1,30 @@
 <template>
-  <div>
-    <h1 class="text-red-600">Twitter clone</h1>
+  <div :class="{ 'dark': darkMode }">
+    <div class="bg-white dark:bg-dim-900">
+      <div class="min-h-full">
+        <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-10 bg-orange-300">
+
+          <div class="md:block xs-col-span-1 xl:col-span-2">
+            <div class="sticky top-0">
+              <SidebarLeft />
+            </div>
+          </div>
+
+          <main class="col-span-11 sm:col-span-8 xl:col-span-6  bg-red-500">
+            <h1>title</h1>
+          </main>
+
+          <div class="hidden md:block xl:col-span-4 md:col-span-3 bg-blue-500">
+            <div class="sticky top-0">
+              <SidebarRight />
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+<script setup>
+const darkMode = ref(true);
+</script>
---
+++ components/Sidebar/Right/PreviewCard/Item.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="p-3 hover:bg-gray-100 dark:hover:bg-dim-300 border-b" :class="wrapperClasses">
+        <slot></slot>
+    </div>
+</template>
+<script setup>
+const { defaultTransition, twitterBorderColor } = useTailwindConfig();
+const wrapperClasses = computed(() => `${defaultTransition} ${twitterBorderColor}`)
+</script>
